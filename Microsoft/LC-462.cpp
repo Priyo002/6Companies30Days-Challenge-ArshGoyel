7a236d2b77ab5bec2736b40b317715b009676fd4
Priyabrata Das <priyobapi@gmail.com>
2024-01-07 21:02:14 +0530

    Day-6 of Arsh Goyal #6Companies30Days Challenge

---
+++ Microsoft/LC-462.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int avg;
+        if (n % 2 == 0) {
+            avg = (nums[n / 2] + nums[n / 2 - 1]) / 2;
+        }
+        else avg = nums[n / 2];
+        int ans = 0;
+        for (auto x : nums) {
+            ans += abs(x - avg);
+        }
+        return ans;
+    }
+};
